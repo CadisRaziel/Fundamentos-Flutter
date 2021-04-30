d90293f63288dd0f627c7ab4898d7c8f6cdee762
vitorbrussolozerbato <vitorcsharp@hotmail.com>
2021-04-29 22:13:07 -0300

    Segunda parte dos fundamentos

--- fundamentos/operadores_4.dart
+++ fundamentos/operadores_4.dart
@@ -8,4 +8,7 @@ main() {
   var estaFrio = stdin.readLineSync() == "s";
 
   String resultado = estaChovendo || estaFrio ? "Ficar em casa" : "Sair";
+  // parte 1 = estaChovendo || estaFrio (vai retornar verdadeiro ou falso dessa expressão)
+  // parte 2 = ? "Ficar em casa" (caso a expressão seja veradeira ele atribui o Ficar em casa)
+  // parte 3 = : "Sair"; (caso a expressão seja falsa ela atribuiu o Sair)
 }
